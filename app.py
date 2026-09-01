fadec71a6ed63826e539d0b9985bdba7dc34d8b0
LuedyCosta <luedy.c@casadeapostas.com>
2026-09-01 13:55:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,10 @@ st.sidebar.title("⚽ Dashboard FC26")
 st.sidebar.markdown("---")
 st.sidebar.markdown("### Navegação")
 
-page_selection = st.sidebar.radio("Ir para:", ["Visão Geral", "Jogadores", "Formações", "Explicando stats"])
+page_selection = st.sidebar.radio(
+    "Navegação",
+    ["Visão Geral", "Jogadores", "Formações", "Explicando stats"] # Adicionado aqui
+)
 
 df = df_raw.copy()
 
