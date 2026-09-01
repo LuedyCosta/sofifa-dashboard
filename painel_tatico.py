b19462782f3ea7cb94226eceb4640b2302345f9b
LuedyCosta <luedy.c@casadeapostas.com>
2026-09-01 10:48:10 -0300

    Create painel_tatico.py

---
+++ painel_tatico.py
@@ -0,0 +1,8 @@
+# Arquivo: painel_tatico.py
+import streamlit.components.v1 as components
+
+def renderizar_painel_tatico():
+    painel_tatico_html = """
+    <!-- Cole todo o bloco HTML/CSS/JS aqui dentro -->
+    """
+    components.html(painel_tatico_html, height=520, scrolling=True)
